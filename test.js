db1a299ea9e92db5b2d9f751cf179731de205fbf
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-09-28 16:04:54 -0400

    connection to obs works -- port needs to match up with obs port, which can be found in obs > tools > websocket

--- test.js
+++ test.js
@@ -1,31 +1,72 @@
-
-const obs = new OBSWebSocket();
-document.getElementById('address_button').addEventListener('click', e => {
-  const address = document.getElementById('address').value;
-
-  obs.connect({
-    address: address
-  });
-});
-obs.on('ConnectionOpened', () => {
-  obs.send('GetSceneList').then(data => {
-    const sceneListDiv = document.getElementById('scene_list');
-    data.scenes.forEach(scene => {
-      const sceneElement = document.createElement('button');
-      sceneElement.textContent = scene.name;
-      sceneElement.onclick = function() {
-        obs.send('SetCurrentScene', {
-          'scene-name': scene.name
-        });
-      };
-      sceneListDiv.appendChild(sceneElement);
-    });
+window.onload = function () {
+  
+  const submitButton = document.querySelector('#addressButton')
+  
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const addressInput = document.getElementById('address').value;
+    console.log(addressInput);
+    const obs = new OBSWebSocket();
+    obs.connect({
+      address: `localhost:${addressInput}`
+    })
+   .then(() => {
+     console.log(`Success! We're connected & authenticated.`);
+   })
   })
-});
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-slider.oninput = function () {
-  output.innerHTML = this.value;
 }
+
+
+
+
+// obs.connect({
+//   address: 'localhost:4444',
+// })
+//   .then(() => {
+//     console.log(`Success! We're connected & authenticated.`);
+//   })
+
+
+
+// handleClick = (e) => {
+// e
+// console.log('bang')
+// }
+
+// const addressButton = document.querySelectorAll('button')
+// addressButton.addEventListener('click', handleClick())
+
+// const slideValue = document.getElementById('myRange')
+// console.log(slideValue)
+
+
+
+// document.getElementById('address_button').addEventListener('click', e => {
+//   console.log('newAddress')
+  // obs.connect({
+  //   address: address
+  // });
+// });
+
+// obs.on('ConnectionOpened', () => {
+//   obs.send('GetSceneList').then(data => {
+//     const sceneListDiv = document.getElementById('scene_list');
+//     data.scenes.forEach(scene => {
+//       const sceneElement = document.createElement('button');
+//       sceneElement.textContent = scene.name;
+//       sceneElement.onclick = function() {
+//         obs.send('SetCurrentScene', {
+//           'scene-name': scene.name
+//         });
+//       };
+//       sceneListDiv.appendChild(sceneElement);
+//     });
+//   })
+// });
+
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+// slider.oninput = function () {
+//   output.innerHTML = this.value;
+// }
